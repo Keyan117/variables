e2a5701aa221f8ce512f49c3237b1a52e5d3da4e
Keyan117 <kdaneshvari98@gmail.com>
2023-03-29 15:18:37 -0700

    variables.cs

---
+++ variables.cs
@@ -0,0 +1,15 @@
+// Declaring and Initialize variables
+string name; = "John";
+int age; = 31;
+char middleintial; "D";
+bool isOver18; = true;
+double currentTemp; = 77.9;
+decimal currentprice; = 29.99m;
+
+// Writing the variables to the console
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isover18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
